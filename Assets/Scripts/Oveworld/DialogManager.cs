210d77db55271d5a602a030ae7d0aa890d47452a
Anaf02 <ana.florescu.02@gmail.com>
2024-07-26 22:29:28 +0200

    dialog manager

---
+++ Assets/Scripts/Oveworld/DialogManager.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    [SerializeField] GameObject dialogBox;
+
+    [SerializeField] Text dialogText;
+
+    [SerializeField] int lettersPerSecond;
+
+    public static DialogManager Instance { get; private set; }
+
+    public event Action OnShowDialog;
+
+    public event Action OnHideDialog;
+
+    Dialog dialog;
+    int currentLine = 0;
+
+    bool isTyping;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void HandleUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Z) && !isTyping)
+        {
+            ++currentLine;
+            if (currentLine < dialog.Lines.Count)
+            {
+                StartCoroutine(TypeDialog(dialog.Lines[currentLine]));
+            }
+            else
+            {
+                dialogBox.SetActive(false);
+                currentLine = 0;
+                OnHideDialog?.Invoke();
+            }
+        }
+    }
+
+    public IEnumerator ShowDialog(Dialog dialog)
+    {
+        yield return new WaitForEndOfFrame();
+
+        OnShowDialog?.Invoke();
+
+        this.dialog = dialog;
+        dialogBox.SetActive(true);
+        StartCoroutine(TypeDialog(dialog.Lines[0]));
+    }
+
+    public IEnumerator TypeDialog(string line)
+    {
+        isTyping = true;
+        dialogText.text = "";
+        foreach (char letter in line.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
+        isTyping = false;
+    }
+}
